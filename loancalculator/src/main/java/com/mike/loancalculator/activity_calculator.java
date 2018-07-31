6a6278fd400af12edaa20bf930beacbd4e1e301d
Mike <clsheng88@gmail.com>
2018-07-31 11:51:07 +0800

    loan calculator completed

---
+++ loancalculator/src/main/java/com/mike/loancalculator/activity_calculator.java
@@ -0,0 +1,13 @@
+package com.mike.loancalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
